80b4bd740bda6d3fee8f14b5d61a0adc672a941c
liuyk <271007729@qq.com>
2021-12-30 01:42:23 +0800

    提交安装supervisor脚本

--- supervisor.py
+++ supervisor.py
@@ -1,9 +1,24 @@
 import utils
 
+file = './libs/supervisor-4.2.3.tar.gz'
+dest = '/supervisor-4.2.3'
+
+
+def mkdirs(config):
+    dir = config.get('supervisor', 'dir')
+    if dir is None:
+        dir = '/opt/supervisor'
+    utils.mkdirs(dir)
+    return dir;
+
 
 def install():
     config = utils.loadConfig()
-    print(config)
+    dir = mkdirs(config)
+    # tar -zxvf
+    utils.unTar(file, dir)
+    # install
+    utils.setup(dir + dest)
 
 
 install()
--- utils.py
+++ utils.py
@@ -1,22 +1,61 @@
 #!/usr/bin/python
 import platform
 import sys
+import os.path
+import subprocess
+
+
+def isPy2():
+    return sys.version_info.major == 2
+
+
+def isPy3():
+    return sys.version_info.major == 3
 
 
 def isLinux():
-    sys = platform.system()
-    return sys == 'Linux'
+    name = platform.system()
+    return name == 'Linux'
 
 
 def isWindow():
-    sys = platform.system()
-    return sys == 'Windows'
+    name = platform.system()
+    return name == 'Windows'
+
+
+def exists(file):
+    return os.path.exists(file)
+
+
+def mkdirs(file):
+    if not exists(file):
+        os.makedirs(file)
+
+
+def execute(command):
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(p.stdout.readlines())
+    for line in p.stdout.readlines():
+        print(line),
+    return p.wait()
+
+
+def unTar(file, dir):
+    execute("tar -zxvf " + file + " -C " + dir)
+
+
+def cd(dir):
+    execute("cd " + dir)
+
+
+def setup(dest):
+    execute('python ' + dest + '/setup.py install')
 
 
 def loadConfig():
-    path = './conf/config.properties'
-    print('load')
-    if (sys.version_info > (3, 0)):
+    # config path
+    path = './conf/config.ini'
+    if isPy3():
         import configparser
         config = configparser.ConfigParser()
         config.read(path)
