d7cc000fd9917c88004a0ad585ec7bac66be5228
liuyk <liuyk@tsingyun.net>
2022-02-14 16:02:17 +0800

    去掉日志

--- monitor/src/timer.js
+++ monitor/src/timer.js
@@ -4,26 +4,26 @@ address = process.argv.splice(2);
 
 function timer() {
   if (address.length <= 0) {
-    console.error("please config monitor address.")
+    // console.error("please config monitor address.")
     return
   }
 
   exec("pm2 jlist", function (error, stdout, stderr) {
     if (error) {
-      console.log(error);
+      // console.log(error);
       return;
     }
 
     if (stderr) {
-      console.log(stderr);
+      // console.log(stderr);
       return;
     }
     axios.post(address[0])
         .then(res => {
-          console.log(res)
+          // console.log(res)
         })
         .catch(err => {
-          console.log(err);
+          // console.log(err);
         });
   });
 }
